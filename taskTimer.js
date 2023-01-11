7866d0ff52456744137d94358dab6d37862fa467
Konstantin Chimev <konsantinchimev119@gmail.com>
2023-01-11 21:02:51 +0200

    Fix
    Fixed hours not updating after 60 minutes have passed.

--- taskTimer.js
+++ taskTimer.js
@@ -9,7 +9,6 @@ var arrMinutes=0;
 function start(){
     let input=document.getElementById("time-taken");
     let userInput=input.value;
-    console.log(userInput);
     var regex=/\d{1,2}[:]\d{1,2}[:]\d{1,2}/;
     if(regex.test(userInput)!=true){
         checkInput();
@@ -18,24 +17,23 @@ function start(){
     else{
         timeArr=userInput.split(":");
     }
-    console.log(userInput);
     // let timeArr=userInput.split(":");
     hours=parseInt(timeArr[0],10);
     minutes=parseInt(timeArr[1],10);
     seconds=parseInt(timeArr[2],10);
+    let totalSeconds=hours*3600+minutes*60+seconds;
     if(document.getElementById("start")!=null){
             
             function startInterval(){
                 interval=setInterval(()=>{
-                seconds++;
-                console.log(seconds);
-                var currentSeconds=seconds%60;
-                var currentMinutes=Math.floor((seconds/60)%60)+minutes;
-                var currentHours=Math.floor(Math.floor(seconds/60)/60)+hours;
-                timeArr[0]=currentHours.toString().padStart(2,"00");
-                timeArr[1]=currentMinutes.toString().padStart(2,"0");
-                timeArr[2]=currentSeconds.toString().padStart(2,"0");
-                updateInterfaceTime();
+                    totalSeconds++;
+                    let currentSeconds=(totalSeconds%60);
+                    let currentMinutes=(Math.floor((totalSeconds%3600)/60));
+                    let currentHours=(Math.floor(totalSeconds/3600));
+                    timeArr[0]=currentHours.toString().padStart(2,"00");
+                    timeArr[1]=currentMinutes.toString().padStart(2,"0");
+                    timeArr[2]=currentSeconds.toString().padStart(2,"0");
+                    updateInterfaceTime();
                 },1000);    
             }
         startInterval();
@@ -109,7 +107,6 @@ function start(){
         for(const key in numberObject){
                 if(key==(inputArr[i])){
                     inputArr[i]=numberObject[key];
-                    console.log(inputArr);
                 }
             }
         }
@@ -119,21 +116,18 @@ function start(){
                 inputArr[k-1]=parseInt(inputArr[k-1],10);
             }
             arrHours=arrHours+inputArr[k-1];
-            console.log(arrHours);
         }
         if(minutesObject.hasOwnProperty(inputArr[k])){
             if(typeof inputArr[k-1]!="number"){
                 inputArr[k-1]=parseInt(inputArr[k-1],10);
             }
             arrMinutes=arrMinutes+inputArr[k-1];
-            console.log(arrMinutes);
         }
         if(secondsObject.hasOwnProperty(inputArr[k])){
             if(typeof inputArr[k-1]!="number"){
                 inputArr[k-1]=parseInt(inputArr[k-1],10);
             }
             arrSeconds=arrSeconds+inputArr[k-1];
-            console.log(arrSeconds);
         }
         }
         userInput=arrHours+":"+arrMinutes+":"+arrSeconds;
